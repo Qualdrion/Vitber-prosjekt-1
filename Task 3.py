a35130097cb45ed6ad83c3f2e5a2cd01e50e74f7
Qualdrion <ebogsnes@gmail.com>
2018-01-29 17:26:33 +0100

    Stuff

--- Task 3.py
+++ Task 3.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import *
+from matplotlib import pyplot as plt
 
 def fredholm_rhs (xc, F):
     '''Set up the RHS of the system
@@ -8,7 +9,7 @@ def fredholm_rhs (xc, F):
         F : function defining the geological survey measurements
     OUTPUT:
         vector defining the RHS of the system'''
-    Nc = xc.shape [0]
+    Nc = len(xc)
     b = np.zeros(Nc)
     for i in range(Nc):
         b[i] = F(xc[i])
@@ -24,8 +25,8 @@ def fredholm_lhs (xc, xs, xq, w, K):
     K: integral kernel
     OUTPUT:
     matrix defining the LHS of the system'''
-    Nc = xc.shape[0]
-    Ns = xs.shape[0]
+    Nc = len(xc)
+    Ns = len(xs)
     A = np.zeros((Nc, Ns))
     #FIXIT : implement the function!
     for i in range(Nc):
@@ -41,5 +42,48 @@ def fredholm_lhs (xc, xs, xq, w, K):
 def Chebyshev(n):
     a = []
     for i in range(1, n+1):
-        a.append(0.5 + 0.5*cos())
-    return
+        a.append(0.5 + 0.5*cos(pi*(2*i-1)/(2*n)))
+    return a
+
+def Trapezoid(n):
+    xq = []
+    w = []
+    for i in range(n+1):
+        xq.append(i/n)
+        if i == 0 or i == n:
+            w.append(0.5)
+        else:
+            w.append(1)
+    return xq, w
+
+def Density(a):
+    p = []
+    for i in range(len(a)):
+        p.append(sin(3*pi*a[i])*exp(-2*a[i]))
+    return p
+
+def Gen_Error(n, p, xc, xs, K, F):
+    y = []
+    for i in range(1, 8):
+        xq, w = Trapezoid(2**i)
+        A = fredholm_lhs(xc, xs, xq, w, K)
+        b = fredholm_rhs(xc, F)
+        Ap = np.dot(A, p).tolist()
+        r = []
+        for j in range(len(Ap)):
+            r.append(Ap[j]-b[j])
+        y.append(max(r))
+    return y
+
+
+def Plot_func(y):
+    x = np.arange(1, len(y)+1)
+    plt.plot(x, y)
+    plt.show()
+
+a = Chebyshev(40)
+p = Density(a)
+K = lambda x, y: 0.025 * (0.025**2 + (y-x)**2)**(-3/2)
+F = lambda x: (1-x)/(0.025*(0.025**2+(x-1)**2)**(1/2)) - (-x)/(0.025*(0.025**2+(x)**2)**(1/2))
+y = Gen_Error(100, p, a, a, K, F)
+Plot_func(y)
